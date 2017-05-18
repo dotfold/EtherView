36c1df518accd697274bf6e01340c80414a5129a
dotfold <james@dotfold.io>
2017-05-18 21:18:22 +1000

    Initial Commit

---
+++ EtherView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EtherView
+//
+//  Created by James McNamee on 18/5/17.
+//  Copyright © 2017 James McNamee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
