ad2289f5ac0c85131bc1486898a9cbbc56a2101a
dotfold <james@dotfold.io>
2017-05-18 23:09:49 +1000

    dep(carthage): lib deps

---
+++ EtherView/ExchangeDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ExchangeDetailViewController.swift
+//  EtherView
+//
+//  Created by James McNamee on 18/5/17.
+//  Copyright © 2017 James McNamee. All rights reserved.
+//
+
+import Foundation
